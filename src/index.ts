5c87f5eeb5e91fafa4addcaf6c2d8d4f4d19d16b
Muhammad Haekal <93haekal@gmail.com>
2018-04-20 14:26:19 +0700

    add express middleware

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,29 @@
+require("dotenv").config();
+
+//import modules
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { User } from "./entity/User";
 import { Todo } from "./entity/Todo";
 import * as express from "express";
+import * as bodyParser from "body-parser";
+import * as logger from "morgan";
+import * as cookieParser from "cookie-parser";
+import * as path from "path";
+import * as cors from "cors";
+
+//import api
+// const accounts = require("./api/accounts");
+//initialize express app
+const app = express();
+
+//use midleware
+app.use(cors());
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
 createConnection()
   .then(async connection => {
@@ -30,13 +51,12 @@ createConnection()
     // // -----------------------
     //
     // console.log("Here you can setup and run express/koa/any other framework.");
-    const app = express();
 
     app.get("/", async (req, res) => {
       const users = await connection.manager.find(User);
       res.send(users);
     });
 
-    app.listen(8000, () => console.log("listening"));
+    app.listen(3000, () => console.log("listening"));
   })
   .catch(error => console.log(error));
